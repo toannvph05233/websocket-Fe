f84a0fb75d592ea6d50ba85d21bd9da0632e9826
toannvph05233 <johntoan98@gmail.com>
2023-01-11 14:38:51 +0700

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Stomp} from "@stomp/stompjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'websocket-Fe';
+  title = 'grokonez';
+  description = 'Angular-WebSocket Demo';
+  message: string = "";
+
+  greetings: string[] = [];
+  disabled = true;
+  name: string | undefined;
+  private stompClient: any;
+
+  constructor() {
+  }
+
+  setConnected(connected: boolean) {
+    this.disabled = !connected;
+
+    if (connected) {
+      this.greetings = [];
+    }
+  }
+
+  connect() {
+    // đường dẫn đến server
+    const socket = new WebSocket('ws://localhost:8080/gkz-stomp-endpoint/websocket');
+    this.stompClient = Stomp.over(socket);
+    const _this = this;
+    this.stompClient.connect({}, function (frame: any) {
+      _this.setConnected(true);
+      console.log('Connected: ' + frame);
+
+      // là chờ xèm thằng server gửi về.
+      _this.stompClient.subscribe('/topic/public', function (hello: any) {
+        _this.showGreeting(JSON.parse(hello.body).greeting);
+      });
+
+    });
+  }
+
+  disconnect() {
+    if (this.stompClient != null) {
+      this.stompClient.disconnect();
+    }
+    this.setConnected(false);
+    console.log('Disconnected!');
+  }
+
+  sendName() {
+    this.stompClient.send(
+      '/gkz/hello',
+      {},
+      // Dữ liệu được gửi đi
+      JSON.stringify({'name': this.name, 'message': this.message})
+    );
+  }
+
+  showGreeting(message: any) {
+    this.greetings.push(message);
+  }
 }
